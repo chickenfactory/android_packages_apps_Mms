6878fcec7831af99e86774851476e8f5791266c2
Ian Ling <ebol4anthr4x@gmail.com>
2014-05-27 13:53:57 -0700

    Mms: Clear notifications and mark messages read in certain situations
    I made these changes to CarbonRom (a fork of CM11), and I
    heard that people were still having these issues in CM11.
    These two commits (merged into one here) fix issues where messages
    in the Mms app were not getting marked as read in certain situations,
    and the notification was not getting cleared when tapped.
    
    More details about these bugs can be found in my original commits to
    Carbon:
    
    https://github.com/CarbonDev/android_packages_apps_Mms/commit/b3a831a6e3019d256163b23870e0f4e0134fce16
    
    https://github.com/CarbonDev/android_packages_apps_Mms/commit/21c851e139c56f387c83e11d12d2a2fa8e0c3ff0
    
    Change-Id: If6d716f71d7b6ee5986665c32c0adfab21085a07

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2527,6 +2527,13 @@ public class ComposeMessageActivity extends Activity
     }
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(!hasFocus) return;
+        mConversation.markAsRead(true);
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
 
